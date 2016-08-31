fac9a0199d7f0a5be2e28a51a218468d468b6e10
Madhu Mohan Nelemane <mmnelemane@suse.com>
2016-08-31 14:42:34 +0200

    Redfish barclamp: Initial draft

---
+++ chef/cookbooks/redfish/metadata.rb
@@ -0,0 +1,24 @@
+#
+# Copyright 2016, SUSE Linux GmBH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Madhu Mohan Nelemane
+#
+maintainer "Crowbar Project"
+maintainer_email "crowbar@googlegroups.com"
+license "Apache 2.0"
+description "Configures RackScale Architecture through Redfish-based APIs"
+long_description "Provides a facility to use Redfish APIs to configure resources. Eg: Rackscale."
+version "0.1"
+depends "utils"
