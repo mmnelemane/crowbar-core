355d112ca330efda48015b9eaf0703e1cfc3023e
Maximilian Meister <mmeister@suse.de>
2016-09-22 15:03:05 +0200

    prefer class methods over instance methods for the crowbar model

--- crowbar_framework/spec/models/api/crowbar_spec.rb
+++ crowbar_framework/spec/models/api/crowbar_spec.rb
@@ -46,81 +46,76 @@ describe Api::Crowbar do
     )
   end
 
-  context "with a successful creation of a crowbar object" do
-    it "checks the type" do
-      expect(subject).to be_an_instance_of(Api::Crowbar)
-    end
-
-    it "has a version" do
-      expect(subject).to respond_to(:version)
-      expect(subject.version).to eq(crowbar_object["version"])
-    end
-  end
-
   context "with a successful status" do
     it "checks the status" do
-      expect(subject).to respond_to(:status)
-      expect(subject.status).to be_a(Hash)
-      expect(subject.status.stringify_keys).to eq(crowbar_object)
+      expect(subject.class).to respond_to(:status)
+      expect(subject.class.status).to be_a(Hash)
+      expect(subject.class.status.stringify_keys).to eq(crowbar_object)
     end
   end
 
   context "with a successful upgrade" do
     it "shows the status of the upgrade" do
-      expect(subject).to respond_to(:upgrade)
-      expect(subject.upgrade.deep_stringify_keys).to eq(crowbar_upgrade_status)
+      expect(subject.class).to respond_to(:upgrade)
+      expect(subject.class.upgrade.deep_stringify_keys).to eq(crowbar_upgrade_status)
     end
 
     it "triggers the upgrade" do
-      allow_any_instance_of(Api::Crowbar).to(
+      allow(Api::Crowbar).to(
         receive_message_chain(:upgrade_script_path, :exist?).
         and_return(true)
       )
 
-      expect(subject.upgrade!).to be true
+      expect(subject.class.upgrade!).to eq(
+        status: :ok,
+        message: ""
+      )
     end
   end
 
   context "with a failed upgrade" do
-    it "fails to trigger the upgrade" do
-      allow_any_instance_of(Api::Crowbar).to(
+    it "cannot find the upgrade script" do
+      allow(Api::Crowbar).to(
         receive_message_chain(:upgrade_script_path, :exist?).
         and_return(false)
       )
 
-      expect(subject.upgrade!).to be false
+      expect(subject.class.upgrade![:status]).to eq(:unprocessable_entity)
+    end
+
+    it "is already upgrading" do
+      allow(Api::Crowbar).to(
+        receive(:upgrading?).and_return(true)
+      )
+
+      expect(subject.class.upgrade![:status]).to eq(:unprocessable_entity)
     end
   end
 
   context "with maintenance updates installed" do
     it "succeeds" do
-      allow(Open3).to(
-        receive(:popen3).
-        with("zypper patch-check").
-        and_return(false)
-      )
-      expect(subject.maintenance_updates_missing?).to be false
+      expect(subject.class.maintenance_updates_installed?).to be true
     end
   end
 
   context "with no maintenance updates installed" do
     it "fails" do
-      allow(Open3).to(
-        receive(:popen3).
-        with("zypper patch-check").
-        and_return(true)
+      # override global allow from spec_helper
+      allow(Api::Crowbar).to(
+        receive(:maintenance_updates_installed?).
+        and_return(false)
       )
-      expect(subject.maintenance_updates_missing?).to be true
+      expect(subject.class.maintenance_updates_installed?).to be false
     end
   end
 
-  context "with addons installed" do
-    it "lists the installed addons" do
-      allow_any_instance_of(Api::Crowbar).to(
-        receive(:addon_installed?).
-        and_return(true)
-      )
+  context "with addons enabled" do
+    it "lists the enabled addons" do
       ["ceph", "ha"].each do |addon|
+        allow(Api::Crowbar).to(
+          receive(:addon_installed?).with(addon).
+          and_return(true)
+        )
         allow(Api::Node).to(
           receive(:repocheck).with(addon: addon).and_return(
             addon => { "available" => true }
@@ -128,34 +123,108 @@ describe Api::Crowbar do
         )
       end
 
-      expect(subject.addons).to eq(["ceph", "ha"])
+      expect(subject.class.addons).to eq(["ceph", "ha"])
     end
   end
 
-  context "with no addons installed" do
+  context "with no addons enabled" do
     it "lists no addons" do
-      expect(subject.addons).to eq([])
+      expect(subject.class.addons).to eq([])
     end
   end
 
   context "with ceph cluster healthy" do
-    it "succeeds" do
-      expect(subject.ceph_healthy?).to be true
+    it "succeeds to check ceph cluster health" do
+      allow(NodeObject).to(
+        receive(:find).with("roles:ceph-mon AND ceph_config_environment:*").
+        and_return([NodeObject.find_node_by_name("testing.crowbar.com")])
+      )
+      allow_any_instance_of(NodeObject).to(
+        receive(:run_ssh_cmd).with("LANG=C ceph health 2>&1").
+        and_return(exit_code: 0, stdout: "HEALTH_OK\n", stderr: "")
+      )
+      expect(subject.class.ceph_healthy?).to be true
     end
   end
 
   context "with ceph cluster not healthy" do
-    it "fails" do
-      allow_any_instance_of(Api::Crowbar).to(
-        receive(:ceph_healthy?).and_return(false)
+    it "fails when checking ceph cluster health" do
+      allow(NodeObject).to(
+        receive(:find).with("roles:ceph-mon AND ceph_config_environment:*").
+        and_return([NodeObject.find_node_by_name("testing.crowbar.com")])
       )
-      expect(subject.ceph_healthy?).to be false
+      allow_any_instance_of(NodeObject).to(
+        receive(:run_ssh_cmd).with("LANG=C ceph health 2>&1").
+        and_return(exit_code: 1, stdout: "HEALTH_ERR\n", stderr: "")
+      )
+      expect(subject.class.ceph_healthy?).to be false
+    end
+
+    it "fails when exit value of ceph check is 0 but stdout still not correct" do
+      allow(NodeObject).to(
+        receive(:find).with("roles:ceph-mon AND ceph_config_environment:*").
+        and_return([NodeObject.find_node_by_name("testing.crowbar.com")])
+      )
+      allow_any_instance_of(NodeObject).to(
+        receive(:run_ssh_cmd).with("LANG=C ceph health 2>&1").
+        and_return(exit_code: 0, stdout: "HEALTH_WARN", stderr: "")
+      )
+      expect(subject.class.ceph_healthy?).to be false
+    end
+  end
+
+  context "with enough compute resources" do
+    it "succeeds to find enough compute nodes" do
+      allow(NodeObject).to(
+        receive(:find).with("roles:nova-compute-kvm").
+        and_return([node, node])
+      )
+      allow(NodeObject).to(
+        receive(:find).with("roles:nova-compute-xen").
+        and_return([node, node])
+      )
+      expect(subject.class.compute_resources_available?).to be true
+    end
+  end
+
+  context "with not enough compute resources" do
+    it "finds there is only one KVM compute node and fails" do
+      allow(NodeObject).to(
+        receive(:find).with("roles:nova-compute-kvm").
+        and_return([node])
+      )
+      expect(subject.class.compute_resources_available?).to be false
+    end
+    it "finds there is only one XEN compute node and fails" do
+      allow(NodeObject).to(
+        receive(:find).with("roles:nova-compute-kvm").
+        and_return([node, node])
+      )
+      allow(NodeObject).to(
+        receive(:find).with("roles:nova-compute-xen").
+        and_return([node])
+      )
+      expect(subject.class.compute_resources_available?).to be false
+    end
+  end
+
+  context "with no compute resources" do
+    it "finds there is no compute node at all" do
+      allow(NodeObject).to(
+        receive(:find).with("roles:nova-compute-kvm").
+        and_return([])
+      )
+      allow(NodeObject).to(
+        receive(:find).with("roles:nova-compute-xen").
+        and_return([])
+      )
+      expect(subject.class.compute_resources_available?).to be true
     end
   end
 
   context "with repositories in place" do
     it "lists the available repositories" do
-      allow_any_instance_of(Api::Crowbar).to(
+      allow(Api::Crowbar).to(
         receive(:repo_version_available?).and_return(true)
       )
       allow_any_instance_of(Kernel).to(
@@ -164,14 +233,17 @@ describe Api::Crowbar do
         ).and_return(crowbar_repocheck_zypper)
       )
 
-      expect(subject.repocheck.deep_stringify_keys).to eq(crowbar_repocheck)
+      expect(subject.class.repocheck.deep_stringify_keys).to eq(crowbar_repocheck)
     end
   end
 
   context "with repositories not in place" do
     it "lists the repositories that are not available" do
-      allow_any_instance_of(Api::Crowbar).to(
+      allow(Api::Crowbar).to(
         receive(:repo_version_available?).and_return(false)
+      )
+      allow(Api::Crowbar).to(
+        receive(:admin_architecture).and_return("x86_64")
       )
       allow_any_instance_of(Kernel).to(
         receive(:`).with(
@@ -179,13 +251,13 @@ describe Api::Crowbar do
         ).and_return(crowbar_repocheck_zypper)
       )
 
-      expect(subject.repocheck.deep_stringify_keys).to_not eq(crowbar_repocheck)
+      expect(subject.class.repocheck.deep_stringify_keys).to_not eq(crowbar_repocheck)
     end
   end
 
   context "with a locked zypper" do
     it "shows an error message that zypper is locked" do
-      allow_any_instance_of(Api::Crowbar).to(
+      allow(Api::Crowbar).to(
         receive(:repo_version_available?).and_return(false)
       )
       allow_any_instance_of(Api::Crowbar).to(
@@ -197,8 +269,9 @@ describe Api::Crowbar do
         ).and_return(crowbar_repocheck_zypper_locked)
       )
 
-      subject.repocheck
-      expect(subject.errors.full_messages.first).to eq(
+      check = subject.class.repocheck
+      expect(check[:status]).to eq(:service_unavailable)
+      expect(check[:message]).to eq(
         Hash.from_xml(crowbar_repocheck_zypper_locked)["stream"]["message"]
       )
     end
